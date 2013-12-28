946e34684bd28b405e2399f216f6d745480d07e6
AndrewMundy <andrew.mundy@ieee.org>
2013-12-28 16:58:59 +0000

    Added custom layout hooks for some windows.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -5,10 +5,16 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
+myManageHook = composeAll
+    [ className =? "Gimp"       --> doFloat
+    , className =? "XCalc"      --> doFloat
+    ]
+
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
            { modMask = mod4Mask     -- Main mod key is the super key
-           , manageHook = manageDocks <+> manageHook defaultConfig
+           , manageHook = manageDocks <+> myManageHook
+                          <+> manageHook defaultConfig
            , layoutHook = avoidStruts $ layoutHook defaultConfig
            }
