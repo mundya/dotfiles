1f2781cfca90219a3a1e6d16e71557e1c5467ff6
AndrewMundy <andrew.mundy@ieee.org>
2013-12-28 15:46:00 +0000

    Always display the status bar.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,13 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
     xmonad $ defaultConfig
-           { modMask = mod4Mask     # Main mod key is the super key
+           { modMask = mod4Mask     -- Main mod key is the super key
+           , manageHook = manageDocks <+> manageHook defaultConfig
+           , layoutHook = avoidStruts $ layoutHook defaultConfig
            }
